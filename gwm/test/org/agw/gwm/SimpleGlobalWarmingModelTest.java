7e9bfed54fd1d8b6879badcf617eb9fce3e5e981
York Earwaker <yorkearwaker@yahoo.com>
2025-01-07 22:02:58 +0000

    test package now matches src

---
+++ gwm/test/org/agw/gwm/SimpleGlobalWarmingModelTest.java
@@ -0,0 +1,14 @@
+package org.agw.gwm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleGlobalWarmingModelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
