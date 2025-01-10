a27c238caa434030b5e1f61cf6fbba2bc10559b5
York Earwaker <yorkearwaker@yahoo.com>
2025-01-10 00:09:39 +0000

    comments refined

--- eea/src/org/agw/eea/EventModel.java
+++ eea/src/org/agw/eea/EventModel.java
@@ -16,8 +16,20 @@ public interface EventModel {
 	 * public abstract method declarations, default
 	 * private abstract method declartions, if any
 	 */
+	 
+	/**
+	 * model of the event
+	 */
 	public void model(); // todo: what is being modelled, model type attributes
+	
+	/**
+	 * temporal defintion of event
+	 */
 	public void time(); // todo: date time, when is this
+	
+	/**
+	 * spacial definition of event
+	 */
 	public void space(); // todo: geo location, where is this, earth another planet
 	
 	/**
