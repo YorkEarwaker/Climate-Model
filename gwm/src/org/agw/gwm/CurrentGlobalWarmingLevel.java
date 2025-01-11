ae631e1fa9335b27f382bf6d4f5511288248d034
York Earwaker <yorkearwaker@yahoo.com>
2025-01-11 13:11:33 +0000

    Met Office paper URI
    Met Office paper Nature on need for current data blended global warming level metric

--- gwm/src/org/agw/gwm/CurrentGlobalWarmingLevel.java
+++ gwm/src/org/agw/gwm/CurrentGlobalWarmingLevel.java
@@ -7,7 +7,7 @@ package org.agw.gwm;
  * CurrentGlobalWarmingLevel
  * Met Office proposed, IPCC compliant, 
  * blended current year mid point, 10 years back data, 10 year future model data
- * 
+ * https://doi.org/10.1038/d41586-023-03775-z
  */
 public interface CurrentGlobalWarmingLevel extends GlobalWarmingLevel {
 
