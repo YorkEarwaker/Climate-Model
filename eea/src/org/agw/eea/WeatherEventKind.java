21aeb2c2f39004bf94437e3320f973e1da9d27f9
York Earwaker <yorkearwaker@yahoo.com>
2025-01-13 12:34:04 +0000

    comments

--- eea/src/org/agw/eea/WeatherEventKind.java
+++ eea/src/org/agw/eea/WeatherEventKind.java
@@ -11,13 +11,13 @@ package org.agw.eea;
  */
 public enum WeatherEventKind {
 	
-	TROPICAL_CYCLONE ("Tropical Cyclone"), # wind storm?,  hurricane/typhoon, storm surge, precipitation, flood, tornadoes, 
-	EXTRA_TROPICAL_CYCLONE ("Extra Tropical Cyclone"), # wind storm?
-	HEAT ("Heat"), # temperature extreme, heat wave
-	COLD ("Cold"), # temperature extreme, cold wave
-	DROUGHT ("Drought"), # meteorological drought, lack of precipitation
-	FLOOD ("Flood"), # excess extreme precipitation
-	CONVECTIVE_STORM ("Convective Storm"); # wind storm?
+	TROPICAL_CYCLONE ("Tropical Cyclone"), // # wind storm?,  hurricane/typhoon, storm surge, precipitation, flood, tornadoes, 
+	EXTRA_TROPICAL_CYCLONE ("Extra Tropical Cyclone"), // # wind storm?
+	HEAT ("Heat"), // # temperature extreme, heat wave
+	COLD ("Cold"), // # temperature extreme, cold wave
+	DROUGHT ("Drought"), // # meteorological drought, lack of precipitation
+	FLOOD ("Flood"), // # excess extreme precipitation
+	CONVECTIVE_STORM ("Convective Storm"); // # wind storm?
 	
 	private final String weatherEvent;
 	
