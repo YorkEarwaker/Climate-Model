42be02e9557e2e800b028c61ca52352d16f35a00
York Earwaker <yorkearwaker@yahoo.com>
2025-01-15 18:03:04 +0000

    derived from eea
    Consider that these are likely generic enough that they will be used in models other than extreme event attribution

---
+++ mdl/src/org/agw/mdl/StatisticalModelKind.java
@@ -0,0 +1,33 @@
+/**
+ * Model
+ * 
+ */
+package org.agw.mdl;
+
+/**
+ * StatisticalModelKind
+ * Kinds of statistical model, enum statistical model kind 
+ * <todo: are these models in the same form used in other than extreme events?  >
+ */
+public enum StatisticalModelKind {
+	
+	DIRAC ("Dirac"), // Dirac delta function <todo: consider refactoring, Dirac does not fit well here. only part of a method as function, >
+	GAUSSIAN ("Gaussian"), // 
+	GPD ("GPD"), // generalised Pareto distribution GPD
+	GEV ("GEV"), // generalised extreme value GEV
+	GUMBLE ("Gumble"), // 
+	GAMMA ("Gamma"); // 
+	
+	private final String statisticalModel;
+	
+	/**
+	* constructor
+	*/
+	private StatisticalModelKind(String statisticalModel) {
+		this.statisticalModel = statisticalModel;
+	}
+	
+	public String getValue() {
+		return(statisticalModel);
+	}
+}
