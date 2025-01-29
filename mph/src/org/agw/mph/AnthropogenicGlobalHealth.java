7a599892b9787a37a4911b2ed0c235d8ff365faa
York Earwaker <yorkearwaker@yahoo.com>
2025-01-29 19:26:55 +0000

    first commit stub

---
+++ mph/src/org/agw/mph/AnthropogenicGlobalHealth.java
@@ -0,0 +1,18 @@
+/**
+ * Medicine and Public Health mph
+ */
+package org.agw.mph;
+
+import org.agw.geo.gts.Anthropocene;
+
+/**
+ * AnthropogenicGlobalHealth
+ * Medicine and public health practice, adaptation and mitigation, due to climate change
+ * Pandemics
+ * 
+ */
+public interface AnthropogenicGlobalHealth extends Anthropocene {
+	 
+	 
+	 
+}
