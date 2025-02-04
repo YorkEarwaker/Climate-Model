a6f39a8589c8d638ae594962e0b9c92ebf7441bc
York Earwaker <yorkearwaker@yahoo.com>
2025-02-04 16:14:23 +0000

    refactored
    name change from AWModel to AWManagement

---
+++ eco/src/org/agw/eco/AnthropogenicWasteManagement.java
@@ -0,0 +1,22 @@
+/**
+ * Ecology concerns, 
+ * waste management, deforestation, zoonotic transfer, pollution, 
+ */
+package org.agw.eco;
+
+import org.agw.geo.gts.Anthropocene;
+
+/**
+ * AnthropogenicWasteManagement
+ * <todo: consider another name? Control, >
+ * Zero waste
+ * todo: provide arguments, consider generics
+ */
+public interface AnthropogenicWasteManagement extends Anthropocene {
+	
+	/**
+	 * @param args
+	 */
+	// public void foo(); // todo: rename method, one of the protocol method declarations, 
+
+}
