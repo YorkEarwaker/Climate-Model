87802bdc9c51778e282f433034cefca848b86c6a
York Earwaker <yorkearwaker@yahoo.com>
2025-02-13 18:23:57 +0000

    first commit
    elements and molecules

---
+++ chm/src/src/org/agw/chm/elm/Niobium.java
@@ -0,0 +1,26 @@
+/**
+ * Chemical elememt
+ */
+package org.agw.chm.elm;
+
+/**
+ * Niobium
+ */
+public record Niobium() implements Element {
+	
+	/**
+	 * Information
+	 */
+	public static final String IUPAC_PIN_NAME = "Niobium"; //
+	
+	/**
+	 * Definition
+	 */
+	
+	/**
+	 * References
+	 */
+	public static final String WIKIPEDIA_URI = "https://en.wikipedia.org/wiki/Niobium";
+	public static final String IUPAC_URI = "to be sourced";
+
+}
---
+++ chm/src/src/org/agw/chm/elm/Oxygen.java
@@ -0,0 +1,26 @@
+/**
+ * Chemical element
+ */
+package org.agw.chm.elm;
+
+/**
+ * Oxygen
+ */
+public record Oxygen() implements Element {
+	
+	/**
+	 * Information
+	 */
+	public static final String IUPAC_PIN_NAME = "Oxygen"; //
+	
+	/**
+	 * Definition
+	 */
+	
+	/**
+	 * References
+	 */
+	public static final String WIKIPEDIA_URI = "https://en.wikipedia.org/wiki/Oxygen";
+	public static final String IUPAC_URI = "to be sourced";
+	
+}
