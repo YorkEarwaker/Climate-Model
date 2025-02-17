7b8080c92eeb28883818501376e12e8c929e53c0
York Earwaker <yorkearwaker@yahoo.com>
2025-02-17 14:37:25 +0000

    new biome
    base for biome nomenclature and taxonomey
    separate from Global 200 WWF

---
+++ eco/src/org/agw/eco/AnthropogenicBiome.java
@@ -0,0 +1,7 @@
+package org.agw.eco;
+
+import org.agw.geo.gts.Anthropocene;
+
+public interface AnthropogenicBiome extends Anthropocene, Biome {
+
+}
---
+++ eco/src/org/agw/eco/NaturalBiome.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package org.agw.eco;
+
+/**
+ * 
+ */
+public interface NaturalBiome extends Biome {
+
+}
