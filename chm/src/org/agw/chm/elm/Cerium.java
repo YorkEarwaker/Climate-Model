623bd61a0707736cc36814bdfd4bba87a484acd2
York Earwaker <yorkearwaker@yahoo.com>
2025-02-19 16:55:27 +0000

    new
    completes period 6

---
+++ chm/src/org/agw/chm/elm/Cerium.java
@@ -0,0 +1,26 @@
+/**
+ * Chemical element
+ */
+package org.agw.chm.elm;
+
+/**
+ * Cerium
+ */
+public record Cerium() implements Element {
+	
+	/**
+	 * Information
+	 */
+	public static final String IUPAC_PIN_NAME = "Cerium"; //
+	
+	/**
+	 * Definition
+	 */
+	
+	/**
+	 * References
+	 */
+	public static final String WIKIPEDIA_URI = "https://en.wikipedia.org/wiki/Cerium";
+	public static final String IUPAC_URI = "to be sourced";
+
+}
