4bf90fab5bbf7011e59d43b369e48caf17ca8e84
York Earwaker <yorkearwaker@yahoo.com>
2025-02-20 15:09:25 +0000

    new
    completes f-block, period 7,

---
+++ chm/src/org/agw/chm/elm/Mendelevium.java
@@ -0,0 +1,26 @@
+/**
+ * Chemical element
+ */
+package org.agw.chm.elm;
+
+/**
+ * Mendelevium
+ */
+public record Mendelevium() implements Element {
+	
+	/**
+	 * Information
+	 */
+	public static final String IUPAC_PIN_NAME = "Mendelevium"; //
+	
+	/**
+	 * Definition
+	 */
+	
+	/**
+	 * References
+	 */
+	public static final String WIKIPEDIA_URI = "https://en.wikipedia.org/wiki/Mendelevium";
+	public static final String IUPAC_URI = "to be sourced";
+
+}
